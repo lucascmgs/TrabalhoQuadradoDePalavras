970aa359d3eb5df0f58dce8d4654080687cb72a2
Lucas <lucascmgsds@gmail.com>
2019-11-02 15:27:49 -0300

    Pronto

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,116 @@
 mod trie;
 
 use trie::Trie;
-use std::collections::HashMap;
 
-
-struct ResolveQuadrado{
+struct ResolveQuadrado {
     quadrado: Vec<Vec<char>>,
     quadrado_otimo: Vec<Vec<char>>,
     conjuntos_palavras: Vec<Vec<String>>,
     palavras_atuais: Vec<String>,
-    prefixos: Trie<char>
+    prefixos: Trie<char>,
 }
 
 impl ResolveQuadrado {
-    fn new () -> ResolveQuadrado{
-        ResolveQuadrado{quadrado: Vec::new(), quadrado_otimo: Vec::new(),
-                                 conjuntos_palavras: Vec::new(), palavras_atuais: Vec::new(), 
-                                 prefixos: Trie::new()}
+    fn new() -> ResolveQuadrado {
+        ResolveQuadrado {
+            quadrado: Vec::new(),
+            quadrado_otimo: Vec::new(),
+            conjuntos_palavras: Vec::new(),
+            palavras_atuais: Vec::new(),
+            prefixos: Trie::new(),
+        }
     }
 
-    fn resolve(&mut self, palavras: &Vec<String>){
-        self.conjuntos_palavras.resize(palavras.len()+1, Vec::new());
+    fn resolve(&mut self, palavras: &Vec<String>) {
+        //Para cada palavra vamos colocá-la em um "bucket" com o tamanho correspondente. Os buckets são as células de índices correspodentes em self.conjuntos_palavras
         for pal in palavras {
-
+            //Se o bucket não existe, damos resize para que seja criado
+            if pal.len() > self.conjuntos_palavras.len() {
+                self.conjuntos_palavras.resize(pal.len() + 1, Vec::new());
+            }
             self.conjuntos_palavras[pal.len()].push(pal.clone());
-                
-            
+            //Inserimos a palavra nova na árvore de prefixos
             self.prefixos.insert(&pal.chars().collect());
         }
-
+        println!("{:?}", self.conjuntos_palavras);
+        //Pegamos o maior índice de bucket para testar se é possível formar um quadrado de palavras com ele
         let mut count = self.conjuntos_palavras.len() - 1;
-        loop{
+        loop {
+            //Se chegamos a 0, que pode ser porque testamos todos os buckets ou nos foi passado 0 palavras, paramos
             if count == 0 {
                 println!("Não é possível formar um quadrado!");
                 break;
             }
+            //Clonamos o bucket de palavras a serem testadas
             self.palavras_atuais = self.conjuntos_palavras[count].clone();
             self.recorre(count);
             if self.quadrado_otimo.len() > 0 {
-                println!("{:?}", self.quadrado_otimo);
+                println!("Temos o quadrado {:?}", self.quadrado_otimo);
                 break;
             } else {
                 self.quadrado.clear();
             }
             count = count - 1;
         }
-
-
     }
 
-    fn recorre(&mut self, index: usize){
-        if self.palavras_atuais.is_empty() && self.quadrado.len() == index{
+    fn checa_prefixos(&mut self, palavra: &String) -> bool{
+        println!("{:?}", self.quadrado);
+        let chars: Vec<char> = palavra.chars().collect();
+        for ind_char in 0..palavra.len() {
+            let mut pref = Vec::new();
+            for ind_pal in 0..self.quadrado.len(){
+                pref.push(self.quadrado[ind_pal][ind_char]);
+            }
+            pref.push(chars[ind_char]);
+            if !self.prefixos.has_prefix(&pref){
+                println!("Não temos o prefixo {:?}", pref);
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn recorre(&mut self, index: usize) {
+        println!("{:?}", self.palavras_atuais);
+        if self.quadrado.len() == index {
             self.quadrado_otimo = self.quadrado.clone();
             return;
         }
 
-
-        for i in 0..self.palavras_atuais.len(){
-            let pal = self.palavras_atuais[i].clone();
-            self.palavras_atuais.pop().unwrap();
-            let mut char_index = 0;
-            let mut can_add = true;
-            for c in pal.chars(){
-                let mut pref = Vec::new();
-                for ind in 0..self.quadrado.len(){
-                    pref.push(self.quadrado[ind][char_index]);
+        if self.palavras_atuais.len() > 0 {
+            let mut ind = self.palavras_atuais.len() - 1;
+            loop {
+                let mut recorreu = false;
+                let pal = self.palavras_atuais.get_mut(ind).unwrap().clone();
+                self.palavras_atuais.remove(ind);
+                
+                if self.checa_prefixos(&pal){
+                    self.quadrado.push(pal.chars().collect());
+                    self.recorre(index);
+                    self.quadrado.pop();
+                    recorreu = true;
                 }
-                pref.push(c);
-                if !self.prefixos.has_prefix(&pref){
-                    can_add = false;
+                self.palavras_atuais.insert(ind, pal);
+                
+                if ind == 0{
                     break;
                 }
-                char_index = char_index + 1;
+                ind = ind - 1;
             }
-
-            if can_add {
-                self.quadrado.push(pal.chars().collect());
-            }
-            self.palavras_atuais.push(pal.clone());
         }
     }
 }
 
-
 fn main() {
-    let palavras = vec![String::from("ama"), String::from("mas"), String::from("asa")];
+    let palavras = vec![
+        String::from("ama"),
+        String::from("tec"),
+        String::from("mas"),
+        String::from("pic"),
+        String::from("asa"),
+    ];
     let mut resolve_quadrado = ResolveQuadrado::new();
     resolve_quadrado.resolve(&palavras);
 }
